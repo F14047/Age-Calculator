65100e9cae03594369342593cdaa0559553368f3
F14047 <f.barcia1618@gmail.com>
2025-09-23 15:41:03 -0300

    Added the initial html, css and js files

---
+++ age_calc.js
@@ -0,0 +1,11 @@
+        // References to elements.
+        const button
+        const input
+        const output 
+    
+        function calculate (string) {
+            /* Calcular edad di`a, mes y anno. */
+            }
+
+
+        Element.addEventListener("click", calculate);
